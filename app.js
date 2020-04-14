1d0634345aa524ba1cea1abc1397bd121cc9071c
Hachem Zarrad <hachemzarrad@gmail.com>
2020-04-14 04:22:03 +0100

    HTTPS

--- app.js
+++ app.js
@@ -31,6 +31,15 @@ connect.then((db) => {
 }, (err) => { console.log(err); });
 
 
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
 
 
 // view engine setup
