9c93be408605ca3906c264012adaf756b4c70d0e
Hachem Zarrad <hachemzarrad@gmail.com>
2020-05-10 08:12:45 +0100

    Integrating Client and Server

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -107,32 +107,29 @@ favoriteRouter.route('/')
 
 favoriteRouter.route('/:dishId')
 .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
-.get(cors.cors, authenticate.verifyUser,  (req,res,next) => {
+.get(cors.cors, authenticate.verifyUser, (req,res,next) => {
     Favorites.findOne({user: req.user._id})
-    .then((favorite) => {
-        if(favorite != null && favorite.dishes != null){
-            for(element of favorite.dishes){
-                if(element._id.equals(req.params.dishId)){
-                    specifiedFavorite = element;
-                }
-            }
+    .then((favorites) => {
+        if (!favorites) {
             res.statusCode = 200;
             res.setHeader('Content-Type', 'application/json');
-            res.json(element);
-        }
-        else if (favorite == null) {
-            err = new Error('Your list of favorites is empty');
-            err.status = 404;
-            return next(err);
+            return res.json({"exists": false, "favorites": favorites});
         }
         else {
-            err = new Error('Dish ' + req.params.dishId + ' not found');
-            err.status = 404;
-            return next(err);            
+            if (favorites.dishes.indexOf(req.params.dishId) < 0) {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                return res.json({"exists": false, "favorites": favorites});
+            }
+            else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                return res.json({"exists": true, "favorites": favorites});
+            }
         }
-        
+
     }, (err) => next(err))
-    .catch((err) => next(err));
+    .catch((err) => next(err))
 })
 .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
     Dishes.findById(req.params.dishId)
